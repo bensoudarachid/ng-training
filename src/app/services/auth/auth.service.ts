20282395be007641469f2438babe9169974b3b2c
Rachid Bensouda <bensourachid@gmail.com>
2019-12-14 20:28:09 +0100

    make sure auth works right. work on jenkins pipeline

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 import { CookieService } from 'ngx-cookie-service'
 
-import { User } from '../../model/user';
-import { ApiConnection } from '../api-connection.service';
+import { User } from '../../model/user'
+import { ApiConnection } from '../api-connection.service'
 @Injectable()
 export class AuthService {
-    
-  constructor(private http: HttpClient, private cookieService: CookieService) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   login(email: string, password: string) {
     // let params = new HttpParams()
@@ -16,47 +15,47 @@ export class AuthService {
       // params,
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa('clientapp:123456')
-      })
+        Authorization: 'Basic ' + btoa('clientapp:123456'),
+      }),
     }
 
     let body = new HttpParams()
     body = body.set('username', email)
-    body = body.set('password', password);
+    body = body.set('password', password)
     body = body.set('grant_type', 'password')
-    
-    return this.http.post(ApiConnection.API_ENDPOINT+'/oauth/token',
-      body.toString()
-      , httpOptions
-    )
 
+    return this.http.post(
+      ApiConnection.API_ENDPOINT + '/oauth/token',
+      body.toString(),
+      httpOptions
+    )
   }
   refreshToken(refreshToken: string): any {
     const httpOptions = {
       // params,
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa('clientapp:123456')
-      })
+        Authorization: 'Basic ' + btoa('clientapp:123456'),
+      }),
     }
     let body = new HttpParams()
     body = body.set('refresh_token', refreshToken)
     body = body.set('grant_type', 'refresh_token')
     console.log('Auth service Post: ' + refreshToken)
-    return this.http.post(ApiConnection.API_ENDPOINT+'/oauth/token',
-      body.toString()
-      , httpOptions
+    return this.http.post(
+      ApiConnection.API_ENDPOINT + '/oauth/token',
+      body.toString(),
+      httpOptions
     )
-
   }
-  logout( jwtToken:string ) {
+  logout(jwtToken: string) {
     const httpOptions = {
       // params,
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer ' + jwtToken
-      })
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + jwtToken,
+      }),
     }
 
     let body = new HttpParams()
@@ -64,10 +63,10 @@ export class AuthService {
     // body = body.set('password', password);
     // body = body.set('grant_type', 'password')
 
-    return this.http.post(ApiConnection.API_ENDPOINT+'/oauth/logout',
-      body.toString()
-      , httpOptions
+    return this.http.post(
+      ApiConnection.API_ENDPOINT + '/oauth/logout',
+      body.toString(),
+      httpOptions
     )
   }
-
 }
